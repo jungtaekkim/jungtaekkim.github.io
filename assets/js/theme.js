ce1cc9fdf2ef451ce4fb2ae972bd9aa07cda7891
jungtaekkim <jungtaek.kim.mail@gmail.com>
2026-08-10 19:18:42 +0000

    Deploying to gh-pages from @ jungtaekkim/jungtaekkim.github.io@913afd7831e5736d1a8a513802537ec43d738328 🚀

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -10,7 +10,6 @@ let toggleTheme = (theme) => {
 
 let setTheme = (theme) => {
   transTheme();
-  setHighlight(theme);
   setGiscusTheme(theme);
 
   if (theme) {
@@ -38,16 +37,6 @@ let setTheme = (theme) => {
           "--global-bg-color"
         ) + "ee", // + 'ee' for trasparency.
     });
-  }
-};
-
-let setHighlight = (theme) => {
-  if (theme == "dark") {
-    document.getElementById("highlight_theme_light").media = "none";
-    document.getElementById("highlight_theme_dark").media = "";
-  } else {
-    document.getElementById("highlight_theme_dark").media = "none";
-    document.getElementById("highlight_theme_light").media = "";
   }
 };
 
